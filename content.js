2acfce083c1549d287d7cb78a5bfc507ff4de173
Kent Leo L. Makibulan <[leomakibulan@gmail.com]>
2017-12-28 18:25:06 +0800

    Initial Commit

---
+++ content.js
@@ -0,0 +1,18 @@
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/shit|fuck|damn|bitch|crap|piss|dick|darn|cock|pussy|fag|asshole|bastard|slut|douche|faggot|cunt|arsehole|bollocks|ass|bloody/gi, '****');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
