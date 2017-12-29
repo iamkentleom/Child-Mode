aafb199073c49f965abd7d9902af3f5ae899c244
Kent Leo L. Makibulan <[leomakibulan@gmail.com]>
2017-12-29 10:06:59 +0800

    Addition of popup

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/shit|fuck|damn|bitch|crap|piss|dick|darn|cock|pussy|fag|asshole|bastard|slut|douche|faggot|cunt|arsehole|bollocks|ass|bloody/gi, '****');
+            var replacedText = text.replace(/shit|fuck|damn|bitch|crap|piss|dick|darn|cock|pussy|fag|asshole|bastard|slut|douche|faggot|cunt|arsehole|bollocks|ass|wth|wtf|bloody/gi, '****');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
